81a2a91ab4a5b3847b72fc8c8bd7e9f904c15005
J0vvy <154846239+J0vvy@users.noreply.github.com>
2024-06-20 16:44:46 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+// Write your code here:
+console.log('---BEGIN PROGRAM-----')
+
+
+
+
+
+
+// Feel free to uncomment the code below to test your function
+console.log('---DECLARE VARIABLES-----')
+
+
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+
+console.log('coolStuff[] ', coolStuff);
+console.log('myStuff[] ', myStuff);
+
+console.log('---BEGIN FUNCTION-----')
+
+function justCoolStuff(myStuff, coolStuff) {
+  
+}
+console.log(justCoolStuff);
+// Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ]
+
